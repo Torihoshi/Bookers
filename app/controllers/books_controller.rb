717370ab543bf08e28ddfb3540b05d1e59ca1b89
Torihoshi <132331394+Torihoshi@users.noreply.github.com>
2023-06-13 17:03:42 +0000

    [Add]showリンク追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,15 +4,28 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
+  #投稿
   def create
     book = Book.new(book_params)
     book.save
     redirect_to '/top'
   end
 
+  #一覧
   def index
     @books = Book.all
   end
+
+  #詳細
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  #修正
+  def edit
+
+  end
+
 
   private
   #　ストロングパラメータ
